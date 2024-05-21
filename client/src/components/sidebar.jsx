f7701033241e7fa88be05874b94f4e272f9e8e22
Aitemaa <ignatiuskarel@gmail.com>
2024-05-21 23:58:14 +0700

    add logout button

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -2,21 +2,30 @@ import { useEffect } from "react";
 import UserList from "./userList";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../features/userListSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar({handleUserSelect}){
+    const nav = useNavigate();
     const userList = useSelector((state) => state.userList.list)
     const dispatch = useDispatch();
 
-
+    const handleLogout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("access_token")
+        nav('/login')
+    }
       useEffect(() => {
         dispatch(fetchUser());
         console.log(userList)
       }, [])
-      
+
     return (
         <div className="w-1/4 border-r border-gray-300">
-        <div className="p-4 border-b border-gray-300">
+        <div className="p-4 border-gray-300">
           <button className="w-full bg-blue-500 text-white py-2 px-4 rounded">New conversation</button>
+        </div>
+        <div className="p-4 border-gray-300">
+          <button onClick={handleLogout} className="w-full bg-blue-500 text-white py-2 px-4 rounded">Logout</button>
         </div>
         <div className="p-4">
           <div className="mb-4">
