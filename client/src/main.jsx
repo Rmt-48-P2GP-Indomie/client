1a016ba3b7353662da7fffb628dd598def786fff
Aitemaa <ignatiuskarel@gmail.com>
2024-05-21 12:11:22 +0700

    setup

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './App.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
